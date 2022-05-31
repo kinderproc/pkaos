8d0388d09a5a2a2091eea8c755b5dd95d9a58d86
algon <kinderproc84@yandex.ru>
2022-05-30 22:50:45 -0700

    Project structure refactored

---
+++ src/main/java/com/algon/pkaos/PkaosApplication.java
@@ -0,0 +1,13 @@
+package com.algon.pkaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkaosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algon/pkaos/PkaosApplicationTests.java
@@ -0,0 +1,11 @@
+package com.algon.pkaos;
+
+import org.junit.jupiter.api.Test;
+
+class PkaosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
