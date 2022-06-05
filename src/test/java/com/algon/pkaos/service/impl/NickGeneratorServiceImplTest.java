0b994563b238b3695cc273ee1dd9ad4796ab4bd8
algon <kinderproc84@yandex.ru>
2022-06-05 03:23:03 -0700

    generating nicks with filters test

--- src/test/java/com/algon/pkaos/service/impl/NickGeneratorServiceImplTest.java
+++ src/test/java/com/algon/pkaos/service/impl/NickGeneratorServiceImplTest.java
@@ -2,9 +2,14 @@ package com.algon.pkaos.service.impl;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class NickGeneratorServiceImplTest {
 
     NickGeneratorServiceImpl nameGeneratorService;
@@ -20,5 +25,17 @@ class NickGeneratorServiceImplTest {
         System.out.println(nick);
         assertFalse(nick.isEmpty());
         assertTrue(nick.length() > 6);
+    }
+
+    @Test
+    void generateNickWithFilters() {
+        String character = "Good";
+        String appearance = "Bald";
+        String name = "Amanda";
+        String nick = nameGeneratorService.generateNick(character, appearance, name, null, null, null);
+        System.out.println(nick);
+        assertThat(nick, containsString(character));
+        assertThat(nick, containsString(appearance));
+        assertThat(nick, containsString(name));
     }
 }
